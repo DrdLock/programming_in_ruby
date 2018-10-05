104dd54c45c18caabe1d04c2c4ef90b6cb62cc3d
DrdLock <Blackburton23@gmail.com>
2018-10-05 16:50:11 -0700

    Finished exercises 2 through 6 in the Arrays chapter

---
+++ Chapters/Arrays/e3_return_example.rb
@@ -0,0 +1,14 @@
+# Question:
+# How do you return example from the following array?
+
+arr = [["test", "hello", "world"], ["example", "mem"]]
+
+# answer1:
+puts arr[1][0]
+
+# answer2:
+puts arr.last.delete(arr.last.first)
+
+
+## Book Solution
+# puts arr.last.first
