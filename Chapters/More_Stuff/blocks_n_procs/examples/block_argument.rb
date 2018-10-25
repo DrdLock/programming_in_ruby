f27574b7aaeadc8396f264270c1ab3a7b8ff7673
DrdLock <Blackburton23@gmail.com>
2018-10-24 17:31:22 -0700

    Started on the blocks and procs section in the final chapter

---
+++ Chapters/More_Stuff/blocks_n_procs/examples/block_argument.rb
@@ -0,0 +1,13 @@
+﻿# This example is taken from the More Stuff Chapter and demonstrates
+# how to pass blocks as arguments into methods
+
+# passing_block.rb
+
+def take_block(&block)
+  block.call
+end
+
+take_block do
+  puts "Block being called in the method!"
+end
+
